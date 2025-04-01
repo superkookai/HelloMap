c8d4b46df00949fb9034265830d273d48a6e89cc
superkookai <weerawut@live.com>
2025-04-01 09:09:11 +0700

    Initial Commit

---
+++ HelloMap/HelloMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMapApp.swift
+//  HelloMap
+//
+//  Created by Weerawut Chaiyasomboon on 01/04/2568.
+//
+
+import SwiftUI
+
+@main
+struct HelloMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
